985bb84fd1789211d003eec43fc080edd69c9535
mivinci <1366723936@qq.com>
2021-02-15 01:05:12 +0800

    all love reserve

---
+++ main.js
@@ -0,0 +1,31 @@
+const firstdate = '2020-06-23 22:18';
+
+function getInterval(init) {
+  const sum = new Date() - Date.parse(init);
+  const seconds = Math.floor((sum / 1000) % 60),
+    minutes = Math.floor((sum / 1000 / 60) % 60),
+    hours = Math.floor((sum / (1000 * 60 * 60)) % 24),
+    days = Math.floor(sum / (1000 * 60 * 60 * 24));
+
+  return [days, hours, minutes, seconds];
+}
+
+function run(firstdate) {
+  const doms = document.getElementsByClassName('time');
+
+  function deamon() {
+    const t = getInterval(firstdate);
+
+    for (let i = 0; i < doms.length; i++) {
+      if (i === 0) {
+        doms[i].innerText = t[i];
+        continue;
+      }
+      doms[i].innerText = ('0' + t[i]).slice(-2);
+    }
+  }
+
+  setInterval(deamon, 1000);
+}
+
+run(firstdate);
